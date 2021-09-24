27134338f89498a70c54d4d72c3fffb02a237625
fahri-rahmanh <fahryrahman162@gmail.com>
2021-09-24 09:04:03 +0700

    Membuat tugas pertemuan 2

---
+++ app/Models/Model_latihan1.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class Model_latihanl extends Model
+{
+    public $nilai1, $nilai2, $hasil;
+
+    public function jumlah($n1 = null, $n2 = null)
+    {
+        $this->nilai1 = $n1;
+        $this->nilai2 = $n2;
+        return $this->nilai1 + $this->nilai2;
+    }
+}
